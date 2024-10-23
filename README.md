5bfb83a5ef5753069741b8bc0e8a9dd00653b399
lohith-vu21csen0100399 <105277685+lohith-vu21csen0100399@users.noreply.github.com>
2024-10-23 22:42:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# INT-NEW
+# **Travel  Tour  Website** 🌏
+
+This project is the main part of **Advanced Software Engineering Course**, the theme of this project is is to create complete responsive travel agency, travel booking and show adventure tour website.
+
+## Motivation
+
+The following are the objectives of this project:
+
+- Work using the Agile Methodology to plan out a project and develop the software.
+- Learn the process of following a given set of System Requirements to develop a software
+
+## Coding Style
+
+This project is divided into two main parts, frontend and backend.
+
+**Front End Technologies**: HTML5, Css3 , Vanilla JavaScript
+
+**Back End Technologies**: PHP , MySQL
+
+**Project Management**: Agile Methodology
+
+## Main Features
+
+- responsive header with toggle menu effect using vanilla JavaScript.
+- responsive home section with touch slider using swiper.js.
+- responsive about section using flexbox.
+- responsive reviews section  with touch slider using swiper.js.
+- responsive packages section using grid.
+- responsive booking form section with PHP and MySQL.
+- responsive footer section using grid.
+
+
+## User Stories
+
+- As a User, I want to navigate the site in a simple and easy way without complication and understand how to deal with the site.
+- As a User, I want to browse services section to choose the right tour for me.
+- As a User, I want to browse packages section to choose the right one and I have the ability to choose between offers.
+- As a User, I want to review the previous opinions of previous customers who made an existing trips.
+- As a User, I want to make a simple booking operations so I can determine the day of arrival and the day of return, and set the destination too.  
---
+++ book.php
@@ -0,0 +1,143 @@
+<?php
+    session_start(); 
+    if(isset($_SESSION['password']) && isset($_SESSION['email']))
+    {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book</title>
+
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" type="text/css">
+</head>
+<body>
+    <!-- Header Section Start -->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!-- Header Section End -->
+    <!-- Book Section Start-->
+    <div class="heading" style="background: url(images/header-bg-3.png) no-repeat">
+        <h1>Book</h1>
+    </div>
+
+    <section class="booking">
+        <h1 class="heading-title">book your trip!</h1>
+
+        <form action="book_form.php" method="post" class="book-form">
+            <div class="flex">
+                <div class="inputBox">
+                    <span>name :</span>
+                    <input type="text" placeholder="enter your name" name="name">
+                </div>
+
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email" placeholder="enter your email" name="email">
+                </div>
+
+                <div class="inputBox">
+                    <span>phone :</span>
+                    <input type="number" placeholder="enter your number" name="phone">
+                </div>
+
+                <div class="inputBox">
+                    <span>address :</span>
+                    <input type="text" placeholder="enter your address" name="address">
+                </div>
+
+                <div class="inputBox">
+                    <span>where to :</span>
+                    <input type="text" placeholder="place you want to visit" name="location">
+                </div>
+
+                <div class="inputBox">
+                    <span>how many :</span>
+                    <input type="number" placeholder="number of guests" name="guests">
+                </div>
+
+                <div class="inputBox">
+                    <span>arrivals :</span>
+                    <input type="date" name="arrivals">
+                </div>
+
+                <div class="inputBox">
+                    <span>leaving :</span>
+                    <input type="date" name="leaving">
+                </div>
+            </div>
+
+            <input type="submit" value="submit" class="btn" name="send">
+        </form>
+    </section>
+    <!-- Book Section End-->
+    <!-- Footer Section Start -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>Package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+123-456-7890</a>
+                <a href="#"><i class="fas fa-phone"></i>+111-222-3333</a>
+                <a href="#"><i class="fas fa-envelope"></i>travel-tour@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>Mansoura, Egypt</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            </div>
+        </div>
+    </section>
+    <!-- Footer Section End -->
+    <!-- swiper js link  -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <!-- custom js file link  -->
+    <script type="text/javascript" src="js/script.js?v=<?php echo time();?>"></script>
+</body>
+</html>
+<?php
+    }
+    else
+    {
+        header("Location:login_form.php");
+        exit();
+    }
+?>
